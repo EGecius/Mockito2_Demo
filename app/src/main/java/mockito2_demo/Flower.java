ad60b1d7aa9fd6d5341adce76db505a6dba544de
Egis <e.gecius@gmail.com>
2016-11-19 13:19:57 +0000

    add spying tests

---
+++ app/src/main/java/mockito2_demo/Flower.java
@@ -0,0 +1,11 @@
+package mockito2_demo;
+
+public class Flower {
+	public void setNumberOfLeafs(final int originalNumberOfLeafs) {
+
+	}
+
+	public int getNumberOfLeafs() {
+		return 0;
+	}
+}
---
+++ app/src/test/java/mockito2_demo/SpyingTests.java
@@ -0,0 +1,34 @@
+package mockito2_demo;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+@RunWith (MockitoJUnitRunner.class)
+public final class SpyingTests {
+
+	private static final int ORIGINAL_NUMBER_OF_LEAFS = 3;
+	private static final int NEW_NUMBER_OF_LEAFS = 5;
+
+	/*spy - I do not understand the purpose of this yet */
+
+	@Test
+	void shouldStubMethodAndCallRealNotStubbedMethod() {
+		Flower realFlower = new Flower();
+		realFlower.setNumberOfLeafs(ORIGINAL_NUMBER_OF_LEAFS);
+
+		Flower flowerSpy = spy(realFlower);
+		willDoNothing().given(flowerSpy).setNumberOfLeafs(anyInt());
+
+		flowerSpy.setNumberOfLeafs(NEW_NUMBER_OF_LEAFS);//stubbed‚ should do nothing
+		verify(flowerSpy).setNumberOfLeafs(NEW_NUMBER_OF_LEAFS);
+		assertEquals(flowerSpy.getNumberOfLeafs(), ORIGINAL_NUMBER_OF_LEAFS);//value was not changed
+	}
+}
