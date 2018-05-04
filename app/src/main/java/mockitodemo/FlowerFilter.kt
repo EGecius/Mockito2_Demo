862f90502cc1a69eb93a65a266cf6bdd8f641065
Egidijus Gecius <e.gecius@gmail.com>
2018-05-04 15:14:39 +0100

    convert FlowerFilter

---
+++ app/src/main/java/mockitodemo/FlowerFilter.kt
@@ -0,0 +1,13 @@
+package mockitodemo
+
+class FlowerFilter {
+
+    fun filterNoOfFlowers(testNumberOfFlowers: Int): Int {
+        return 0
+    }
+
+    fun tellMeFlowerName(): String? {
+        return null
+    }
+
+}
